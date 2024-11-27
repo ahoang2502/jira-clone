668d61ff77af8dba3211384883b0ba6377295811
Anh Hoang <athoang252@gmail.com>
2024-11-27 14:25:49 +1100

    feat: auth ui complete

--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -114,6 +115,19 @@ export const SignInCard = () => {
           <FaGithub className="mr-2 size-5" />
           Login with Github
         </Button>
+      </CardContent>
+
+      <div className="px-7 ">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7 flex items-center justify-center">
+        <p className="">
+          Don't have an account?{" "}
+          <Link href="/sign-up">
+            <span className="text-blue-700">Sign up</span>
+          </Link>
+        </p>
       </CardContent>
     </Card>
   );
--- src/features/auth/components/SignUpCard.tsx
+++ src/features/auth/components/SignUpCard.tsx
@@ -153,6 +153,19 @@ export const SignUpCard = () => {
           Login with Github
         </Button>
       </CardContent>
+
+      <div className="px-7 ">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7 flex items-center justify-center">
+        <p className="">
+          Already have an account?{" "}
+          <Link href="/sign-in">
+            <span className="text-blue-700">Sign in</span>
+          </Link>
+        </p>
+      </CardContent>
     </Card>
   );
 };
