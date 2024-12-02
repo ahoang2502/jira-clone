cf39a7f8c5c63a5438a21ee64d06be2f97ac46e2
Anh Hoang <athoang252@gmail.com>
2024-12-02 20:01:45 +1100

    feat: appwrite setup + register api

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE="ah-jira-cl-session"
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
+import { ID } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
+import { setCookie } from "hono/cookie";
 
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
@@ -13,7 +17,20 @@ const app = new Hono()
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { email, password, name } = await c.req.valid("json");
 
-    return c.json({ email, password, name });
+    const { account } = await createAdminClient();
+    const user = await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ data: user });
   });
 
 export default app;
