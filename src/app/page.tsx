af976281294c5ee087af73cdeffd573a274cc315
Anh Hoang <athoang252@gmail.com>
2024-12-11 15:35:00 +1100

    feat: get current user api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useEffect } from "react";
+
+import { useCurrent } from "@/features/auth/api/useCurrent";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">destructive</Button>
-      <Button variant="ghost">ghost</Button>
-      <Button variant="outline">outline</Button>
-      <Button variant="muted">muted</Button>
-    </div>
-  );
+  const router = useRouter();
+
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return <div className="">Only visible to authorized users.</div>;
 }
