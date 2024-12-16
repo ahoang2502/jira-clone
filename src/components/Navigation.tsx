81ff2a37e5f59b0dadda0d57460d9e58589eac93
Anh Hoang <athoang252@gmail.com>
2024-12-16 11:59:36 +1100

    feat: dashboard layout: navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,58 @@
-import React from "react";
+import { SettingsIcon, UsersIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  GoCheckCircle,
+  GoCheckCircleFill,
+  GoHome,
+  GoHomeFill,
+} from "react-icons/go";
+
+import { cn } from "@/lib/utils";
+
+const routes = [
+  { label: "Home", href: "/", icon: GoHome, activeIcon: GoHomeFill },
+  {
+    label: "My Tasks",
+    href: "/tasks",
+    icon: GoCheckCircle,
+    activeIcon: GoCheckCircleFill,
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: SettingsIcon,
+    activeIcon: SettingsIcon,
+  },
+  {
+    label: "Members",
+    href: "/members",
+    icon: UsersIcon,
+    activeIcon: UsersIcon,
+  },
+];
 
 export const Navigation = () => {
-  return <div></div>;
+  return (
+    <ul className="flex flex-col">
+      {routes.map((item) => {
+        const isActive = false;
+        const Icon = isActive ? item.activeIcon : item.icon;
+
+        return (
+          <Link href={item.href} key={item.href}>
+            <div
+              className={cn(
+                "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
+                isActive &&
+                  "bg-white shadow-sm opacity-75 hover:opacity-100 text-primary"
+              )}
+            >
+              <Icon className="size-5 text-neutral-500" />
+              {item.label}
+            </div>
+          </Link>
+        );
+      })}
+    </ul>
+  );
 };
