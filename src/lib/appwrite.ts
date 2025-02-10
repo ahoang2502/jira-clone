bf4eb0f5f61b563fe6e83acabfcd53f2e7edd7bf
Anh Hoang <athoang252@gmail.com>
2025-02-10 23:08:04 +1100

    feat: get workspace members api

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
 import { cookies } from "next/headers";
-import { Account, Client, Databases } from "node-appwrite";
+import { Account, Client, Databases, Users } from "node-appwrite";
 
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
@@ -11,7 +11,7 @@ export async function createSessionClient() {
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
   const session = await cookies().get(AUTH_COOKIE);
-  if (!session || !session.value) throw new Error("Unauthorized");
+  if (!session?.value) throw new Error("Unauthorized");
 
   client.setSession(session.value);
 
@@ -34,6 +34,9 @@ export async function createAdminClient() {
   return {
     get account() {
       return new Account(client);
+    },
+    get users() {
+      return new Users(client);
     },
   };
 }
