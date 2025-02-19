7130fdfda34edb144acb83f82ac3ca40fe6bb7b8
Anh Hoang <athoang252@gmail.com>
2025-02-19 14:34:07 +1100

    feat: update & delete member api + hooks

--- src/features/members/server/route.ts
+++ src/features/members/server/route.ts
@@ -7,8 +7,8 @@ import { DATABASE_ID, MEMBERS_ID } from "@/config";
 import { createAdminClient } from "@/lib/appwrite";
 import { sessionMiddleware } from "@/lib/session-middleware";
 
-import { getMember } from "../utils";
 import { MemberRole } from "../types";
+import { getMember } from "../utils";
 
 const app = new Hono()
   .get(
@@ -72,16 +72,60 @@ const app = new Hono()
 
     if (!member) return c.json({ error: "Unauthorized" }, 401);
 
+    if (member.id !== memberToDelete.$id && member.role !== MemberRole.ADMIN)
+      return c.json({ error: "Unauthorized" }, 401);
+
     if (allMembersInWorkspace.total === 1) {
       return c.json({ error: "Cannot delete the only member" }, 400);
     }
 
-    if (member.id !== memberToDelete.$id && member.role !== MemberRole.ADMIN)
-      return c.json({ error: "Unauthorized" }, 401);
-
     await databases.deleteDocument(DATABASE_ID, MEMBERS_ID, memberId);
 
     return c.json({ data: { $id: memberToDelete.$id } });
-  });
+  })
+  .patch(
+    "/:memberId",
+    sessionMiddleware,
+    zValidator("json", z.object({ role: z.nativeEnum(MemberRole) })),
+    async (c) => {
+      const { memberId } = c.req.param();
+      const { role } = c.req.valid("json");
+      const user = c.get("user");
+      const databases = c.get("databases");
+
+      const memberToUpdate = await databases.getDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        memberId
+      );
+
+      const allMembersInWorkspace = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        [Query.equal("workspaceId", memberToUpdate.workspaceId)]
+      );
+
+      const member = await getMember({
+        databases,
+        workspaceId: memberToUpdate.workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) return c.json({ error: "Unauthorized" }, 401);
+
+      if (member.role !== MemberRole.ADMIN)
+        return c.json({ error: "Unauthorized" }, 401);
+
+      if (allMembersInWorkspace.total === 1) {
+        return c.json({ error: "Cannot downgrade the only member" }, 400);
+      }
+
+      await databases.updateDocument(DATABASE_ID, MEMBERS_ID, memberId, {
+        role,
+      });
+
+      return c.json({ data: { $id: memberToUpdate.$id } });
+    }
+  );
 
 export default app;
