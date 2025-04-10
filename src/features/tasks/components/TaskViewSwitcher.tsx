55da1ed8e24a9f03e118ca46598b9aede39c0486
Anh Hoang <athoang252@gmail.com>
2025-04-10 18:12:37 +1000

    feat: url tags

--- src/features/tasks/components/TaskViewSwitcher.tsx
+++ src/features/tasks/components/TaskViewSwitcher.tsx
@@ -1,18 +1,32 @@
 "use client";
 
 import { PlusIcon } from "lucide-react";
+import { useQueryState } from "nuqs";
 
 import { DottedSeparator } from "@/components/Dotted-Separator";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import { useGetTasks } from "../api/useGetTasks";
 import { useCreateTaskModal } from "../hooks/useCreateTaskModal";
+import { useWorkspaceId } from "@/features/workspaces/hooks/useWorkspaceId";
 
 export const TaskViewSwitcher = () => {
+  const [view, setView] = useQueryState("task-view", { defaultValue: "table" });
+
   const { open } = useCreateTaskModal();
+  const workspaceId = useWorkspaceId();
+
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+  });
 
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs
+      className="flex-1 w-full border rounded-lg"
+      defaultValue={view}
+      onValueChange={setView}
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
@@ -39,13 +53,13 @@ export const TaskViewSwitcher = () => {
         <DottedSeparator className="my-4" />
         <>
           <TabsContent value="table" className="mt-0">
-            Data table
+            {JSON.stringify(tasks)}
           </TabsContent>
           <TabsContent value="kanban" className="mt-0">
-            Data kanban
+            {JSON.stringify(tasks)}
           </TabsContent>
           <TabsContent value="calendar" className="mt-0">
-            Data calendar
+            {JSON.stringify(tasks)}
           </TabsContent>
         </>
       </div>
