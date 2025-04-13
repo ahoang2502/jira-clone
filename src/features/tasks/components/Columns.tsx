2cee9c90385c05edf2eab35ef07fc499bd9b7a9e
Anh Hoang <athoang252@gmail.com>
2025-04-13 22:19:47 +1000

    feat: tanstack table setup

---
+++ src/features/tasks/components/Columns.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Task } from "../types";
+
+export const columns: ColumnDef<Task>[] = [
+  { accessorKey: "name", header: "Task Name" },
+];
+
+export const Columns = () => {
+  return <div></div>;
+};
