b49fe94b5c5e3fec3954aabda0884c197fae1f8e
Anh Hoang <athoang252@gmail.com>
2025-04-19 23:42:56 +1000

    feat: edit task form modal + wrapper

---
+++ src/features/tasks/components/EditTaskFormWrapper.tsx
@@ -0,0 +1,56 @@
+import { Loader } from "lucide-react";
+
+import { Card, CardContent } from "@/components/ui/card";
+
+import { useGetMembers } from "@/features/members/api/useGetMembers";
+import { useGetProjects } from "@/features/projects/api/useGetProjects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/useWorkspaceId";
+import { CreateTaskForm } from "./CreateTaskForm";
+
+interface EditTaskFormWrapperProps {
+  onCancel: () => void;
+  id: string;
+}
+
+export const EditTaskFormWrapper = ({
+  onCancel,
+  id,
+}: EditTaskFormWrapperProps) => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const projectOptions = projects?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+    imageUrl: project.imageUrl,
+  }));
+  const memberOptions = members?.documents.map((member) => ({
+    id: member.$id,
+    name: member.name,
+  }));
+
+  const isLoading = isLoadingMembers || isLoadingProjects;
+
+  if (isLoading)
+    return (
+      <Card className="w-full h-[714px] border-none shadow-none">
+        <CardContent className="flex items-center justify-center h-full">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </CardContent>
+      </Card>
+    );
+
+  return (
+    <CreateTaskForm
+      onCancel={onCancel}
+      projectOptions={projectOptions ?? []}
+      memberOptions={memberOptions ?? []}
+    />
+  );
+};
