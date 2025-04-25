ab4bf09e9e8cc54d0b44f4f2bc0d62a206e29869
Anh Hoang <athoang252@gmail.com>
2025-04-25 19:00:22 +1000

    feat: calendar view - toolbar styling

--- src/features/tasks/components/DataCalendar.tsx
+++ src/features/tasks/components/DataCalendar.tsx
@@ -9,9 +9,11 @@ import {
 import { enUS } from "date-fns/locale";
 import { useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { CalendarIcon, ChevronLeft, ChevronRight } from "lucide-react";
 
 import { Task } from "../types";
 import { EventCard } from "./EventCard";
+import { Button } from "@/components/ui/button";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./data-calendar.css";
@@ -27,6 +29,38 @@ const localizer = dateFnsLocalizer({
   getDay,
   locales,
 });
+
+interface CustomToolbarProps {
+  date: Date;
+  onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+}
+
+const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
+  return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+      <Button
+        onClick={() => onNavigate("PREV")}
+        variant="secondary"
+        size="icon"
+      >
+        <ChevronLeft className="size-4" />
+      </Button>
+
+      <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+        <CalendarIcon className="size-4 mr-2" />
+        <p className="text-sm ">{format(date, "MMMM yyyy")}</p>
+      </div>
+
+      <Button
+        onClick={() => onNavigate("NEXT")}
+        variant="secondary"
+        size="icon"
+      >
+        <ChevronRight className="size-4" />
+      </Button>
+    </div>
+  );
+};
 
 interface DataCalendarProps {
   data: Task[];
@@ -47,7 +81,7 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
     id: task.$id,
   }));
 
-  const handleNavigate = (action: "PREV" | "NEXT" | "TODY") => {
+  const handleNavigate = (action: "PREV" | "NEXT" | "TODAY") => {
     if (action === "PREV") setValue(subMonths(value, 1));
     else if (action === "NEXT") setValue(addMonths(value, 1));
     else setValue(new Date());
@@ -77,6 +111,9 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
             project={event.project}
             status={event.status}
           />
+        ),
+        toolbar: () => (
+          <CustomToolbar date={value} onNavigate={handleNavigate} />
         ),
       }}
     />
