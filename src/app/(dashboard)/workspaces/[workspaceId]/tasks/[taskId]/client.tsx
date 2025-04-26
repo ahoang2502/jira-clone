1eb3c07a2417196fbf0af2c8045c944843985a52
Anh Hoang <athoang252@gmail.com>
2025-04-26 18:24:02 +1000

    feat: task description comp

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -6,6 +6,7 @@ import { PageLoader } from "@/components/PageLoader";
 
 import { useGetTask } from "@/features/tasks/api/useGetTask";
 import { TaskBreadcrumbs } from "@/features/tasks/components/TaskBreadcrumbs";
+import { TaskDescription } from "@/features/tasks/components/TaskDescription";
 import { TaskOverview } from "@/features/tasks/components/TaskOverview";
 import { useTaskId } from "@/features/tasks/hooks/useTaskId";
 
@@ -24,6 +25,7 @@ export const TaskIdClient = () => {
       <DottedSeparator className="my-6" />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <TaskOverview task={data} />
+        <TaskDescription task={data} />
       </div>
     </div>
   );
