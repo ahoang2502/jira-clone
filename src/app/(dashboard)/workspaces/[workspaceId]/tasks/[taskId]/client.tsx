908bf2ae5556448cdd931d406357d636b53d1da1
Anh Hoang <athoang252@gmail.com>
2025-04-26 18:11:06 +1000

    feat: task overview comp

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { DottedSeparator } from "@/components/Dotted-Separator";
 import { PageError } from "@/components/PageError";
 import { PageLoader } from "@/components/PageLoader";
 
 import { useGetTask } from "@/features/tasks/api/useGetTask";
 import { TaskBreadcrumbs } from "@/features/tasks/components/TaskBreadcrumbs";
+import { TaskOverview } from "@/features/tasks/components/TaskOverview";
 import { useTaskId } from "@/features/tasks/hooks/useTaskId";
 
 export const TaskIdClient = () => {
@@ -18,6 +20,11 @@ export const TaskIdClient = () => {
   return (
     <div className="flex flex-col">
       <TaskBreadcrumbs project={data.project} task={data} />
+
+      <DottedSeparator className="my-6" />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <TaskOverview task={data} />
+      </div>
     </div>
   );
 };
