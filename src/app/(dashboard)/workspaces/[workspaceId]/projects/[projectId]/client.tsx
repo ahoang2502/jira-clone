a7d337257f7e6c640fad67115434303895fa6e13
Anh Hoang <athoang252@gmail.com>
2025-04-27 18:57:14 +1000

    feat: get project api  & refactor project client

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -3,32 +3,39 @@
 import { PencilIcon } from "lucide-react";
 import Link from "next/link";
 
+import { PageError } from "@/components/PageError";
+import { PageLoader } from "@/components/PageLoader";
 import { Button } from "@/components/ui/button";
 
+import { useGetProject } from "@/features/projects/api/useGetProject";
 import { ProjectAvatar } from "@/features/projects/components/ProjectAvatar";
-import { TaskViewSwitcher } from "@/features/tasks/components/TaskViewSwitcher";
 import { useProjectId } from "@/features/projects/hooks/useProjectId";
+import { TaskViewSwitcher } from "@/features/tasks/components/TaskViewSwitcher";
 
 const ProjectIdClient = () => {
   const projectId = useProjectId();
-  const {} = useGetProject();
+  const { data, isLoading } = useGetProject({ projectId });
+
+  if (isLoading) return <PageLoader />;
+
+  if (!data) return <PageError message="Project not found" />;
 
   return (
     <div className="flex flex-col gap-y-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-x-2">
           <ProjectAvatar
-            name={initialValues.name}
-            image={initialValues.imageUrl}
+            name={data.name}
+            image={data.imageUrl}
             className="size-8"
           />
-          <p className="text-lg font-semibold">{initialValues.name}</p>
+          <p className="text-lg font-semibold">{data.name}</p>
         </div>
 
         <div className="">
           <Button variant="secondary" size="sm" asChild>
             <Link
-              href={`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}/settings`}
+              href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}
             >
               <PencilIcon className="size-4 mr-2" />
               Edit Project
