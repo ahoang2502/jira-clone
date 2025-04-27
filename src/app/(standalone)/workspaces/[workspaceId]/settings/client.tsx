153e05a5158fb3183ec4abd8382b7fe2ffe2664a
Anh Hoang <athoang252@gmail.com>
2025-04-27 19:09:57 +1000

    refactor: workspace settings page

---
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/client.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { PageError } from "@/components/PageError";
+import { PageLoader } from "@/components/PageLoader";
+
+import { useGetWorkspace } from "@/features/workspaces/api/useGetWorkspace";
+import { EditWorkspaceForm } from "@/features/workspaces/components/EditWorkspaceForm";
+import { useWorkspaceId } from "@/features/workspaces/hooks/useWorkspaceId";
+
+const WorkspaceIdSettingsClient = () => {
+  const workspaceId = useWorkspaceId();
+  const { data: initialValues, isLoading } = useGetWorkspace({ workspaceId });
+
+  if (isLoading) return <PageLoader />;
+
+  if (!initialValues) return <PageError message="Workspace not found" />;
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <EditWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
+};
+
+export default WorkspaceIdSettingsClient;
